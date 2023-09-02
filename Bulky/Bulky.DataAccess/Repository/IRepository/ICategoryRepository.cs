05c63e6e72d571d955cf6285ca993952dc495563
xurcotesting <khurtsilava.nika22@gtu.ge>
2023-09-02 16:52:43 +0400

    need help here
    need help here

---
+++ Bulky/Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Bulky.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+         
+        
+    }
+}
